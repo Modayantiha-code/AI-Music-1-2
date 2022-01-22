d54ebdd243d2a5cad75bdd9f02a60c0df958831d
Modayantiha-code <84634224+Modayantiha-code@users.noreply.github.com>
2022-01-22 13:59:19 -0600

    Add files via upload

--- music.js
+++ music.js
@@ -1,5 +1,9 @@
 var PeterPan = "";
 var HarryPotter = "";
+LeftwristX = 0;
+LeftwristY = 0;
+RightwristX = 0;
+RightwristY = 0;
 
 function preload() {
     PeterPan = loadSound("PeterPan.mp3");
@@ -11,8 +15,26 @@ function setup() {
     mycanvas.center();
     webcam = createCapture(VIDEO);
     webcam.hide();
+    posenetmodel = ml5.poseNet(video, modelloaded);
+    posenetmodel.on("pose", getResults);
+}
+
+function modelloaded() {
+    console.log("Posenet has loaded");
 }
 
 function draw() {
     image(webcam, 0, 0, 500, 350);
+}
+
+function getResults(resultsarray) {
+    if (resultsarray.length > 0) {
+        console.log(resultsarray);
+        LeftwristX = resultsarray[0].pose.leftWrist.x;
+        LeftwristY = resultsarray[0].pose.leftWrist.y;
+        RightwristX = resultsarray[0].pose.rightWrist.x;
+        RightwristY = resultsarray[0].pose.rightWrist.y;
+
+    }
+
 }
